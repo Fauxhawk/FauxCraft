04062e84adaf995e9967364c0f05c2b32bbbac6b
Fauxhawk <fauxhawk2015@gmail.com>
2015-09-08 02:10:44 -0500

    update

---
+++ src/main/java/com/fauxhawk/main/main.java
@@ -0,0 +1,8 @@
+package com.fauxhawk.main;
+
+import net.minecraftforge.fml.common.Mod;
+import com.fauxhawk.Reference.Reference;
+@Mod(modid=Reference.MODID, name=Reference.NAME, version=Reference.VERSION)
+public class main {
+
+}
